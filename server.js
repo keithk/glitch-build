26732c6efba7b1c458acaf815ed7a76fac0ef696
Glitch (talent) <none>
2020-05-28 18:24:14 +0000

    Import of keithk/glitch-alumni ./package.json:617503/26

---
+++ server.js
@@ -0,0 +1,28 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+const path = require('path')
+
+// Create our server
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'build'),
+  prefix: '/',
+})
+
+// And set up our root path, that pulls from /build
+fastify.get('/', function (req, reply) {
+  reply.sendFile('index.html')
+})
+
+
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
